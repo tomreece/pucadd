39d5790161d12f1266de03e961095f4d5c149a91
Tom Reece <tomreece@gmail.com>
2016-02-07 19:26:32 -0600

    Also make page width 100%

--- pucadd.js
+++ pucadd.js
@@ -1,6 +1,7 @@
 $('#frm_search').hide();
+$('#wrap').width('100%');
 $('.search-results').width('100%');
-$('.search-results').prepend('<h1 style="color:#16a085;">You are using PucAdd 1.0</h1><h3 style="color:#27ae60;">Now you can simply click a card to add it to your Haves list.</h3><p><a href="https://github.com/tomreece/pucadd" target="_blank">https://github.com/tomreece/pucadd</a></p>');
+$('.search-results').prepend('<h1 style="color:#16a085;">You are using PucAdd 1.1</h1><h3 style="color:#27ae60;">Now you can simply click a card to add it to your Haves list.<br/>Use your browser\'s zoom controls to make the cards even smaller if you\'d like!</h3><p><a href="https://github.com/tomreece/pucadd" target="_blank">https://github.com/tomreece/pucadd</a></p>');
 
 var resizeAndRebind = function () {
     $('.card-image img').css('max-width', 160);
